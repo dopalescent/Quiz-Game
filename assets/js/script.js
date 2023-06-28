edb7e7da6e07604d8bc543f0a2d293f8c758d654
dopalescent <skudlgre000@gmail.com>
2023-06-28 02:17:12 -0400

    full navigation added, removed console.log's and sandbox

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
-// var hideoptions = document.querySelector("#options");
-var nextButton = document.querySelector("#next-button");
+var startPage = document.querySelector('#start-page');
+var startButton = document.querySelector('#start-button');
+var scoreLink = document.querySelector('#score-link');
+var quizPage = document.querySelector('#quiz-page');
+var quizButton = document.querySelector("#quiz-button");
+var victoryPage = document.querySelector('#victory-page');
+var victoryText = document.querySelector('#victory-text');
+var victoryButton = document.querySelector('#victory-button');
+var scorePage = document.querySelector('#score-page');
+var scoreButton = document.querySelector('#score-button');
+
 var timerEl = document.getElementById('timer');
-var questionText = document.querySelector('#question-text')
+var questionText = document.querySelector('#question-text');
 var options = document.querySelectorAll('li');
 var op1 = document.querySelector('#q1');
 var op2 = document.querySelector('#q2');
@@ -22,72 +31,88 @@ var correct
 var currentQuestion = 0;
 
 
+function setStart() {
+  scorePage.setAttribute('style', 'display:none');
+  startPage.setAttribute('style', 'display:block');
+  quizPage.setAttribute('style', 'display:none');
+  victoryPage.setAttribute('style', 'display:none');
+};
+
+function setQuiz() {
+  startPage.setAttribute('style', 'display:none');
+  quizPage.setAttribute('style', 'display:block');
+  quizButton.setAttribute('style', 'display:none');
+  setUp();
+};
+
+function setVictory() {
+  quizPage.setAttribute('style', 'display:none');
+  victoryPage.setAttribute('style', 'display:block');
+};
+
+function setScore() {
+  startPage.setAttribute('style', 'display:none');
+  victoryPage.setAttribute('style', 'display:none');
+  scorePage.setAttribute('style', 'display:block');
+};
+
 function setUp() {
-  if(currentQuestion >= questions.length){
-    feedbackEl.textContent = "Finished!"
+  if(currentQuestion >= questions.length) {
+    setVictory();
     return;
+  } else if(currentQuestion === questions.length - 1) {
+    quizButton.textContent = "Finish!";
   };
-  console.log(currentQuestion);
   questionText.textContent = questions[currentQuestion];
   var currentOps = opsPool[currentQuestion];
   for (var i = 0; i < options.length; i++) {
     options[i].textContent = currentOps[i];
-    console.log("setup read");
   }
   correct = answerKey[currentQuestion];
-  nextButton.setAttribute('style', 'display:none')
-}
+  quizButton.setAttribute('style', 'display:none');
+};
 
 function reset() {
-  console.log("transition confirmed");
   feedbackEl.textContent = "";
   for (var i = 0; i < options.length; i++) {
     options[i].setAttribute('style', 'color:black; background-color:none;');
-  }
+  };
   attempted = false;
-  currentQuestion++
+  currentQuestion++;
 };
 
 function nextQuestion() {
   reset();
   setUp();
-}
+};
 
 function answer(event) {
   event.stopPropagation();
-  console.log("answer click registered");
-  console.log(event.target);
   if(attempted) {
     console.log("re-answer prevented");
     return;
-  }
+  };
   attempted = true;
-  correct.setAttribute('style', 'color:white; background-color:green;')
+  correct.setAttribute('style', 'color:white; background-color:green;');
   if(event.target === correct) {
-    console.log("correct clicked");
-    feedbackEl.textContent = "Correct!"
+    feedbackEl.textContent = "Correct!";
   } else {
-    event.target.setAttribute('style', 'color:white; background-color:red;')
-    feedbackEl.textContent = "Nope!"
-  }
-  nextButton.setAttribute('style', 'display:block')
-}
+    event.target.setAttribute('style', 'color:white; background-color:red;');
+    feedbackEl.textContent = "Nope!";
+  };
+  quizButton.setAttribute('style', 'display:block');
+};
 
-setUp();
+setStart();
 
-nextButton.addEventListener("click", nextQuestion);
+startButton.addEventListener("click", setQuiz);
+scoreLink.addEventListener("click", setScore);
+quizButton.addEventListener("click", nextQuestion);
+victoryButton.addEventListener("click", setScore);
+scoreButton.addEventListener("click", setStart);
 
 op1.addEventListener("click", answer); 
 op2.addEventListener("click", answer);
 op3.addEventListener("click", answer);
 op4.addEventListener("click", answer);
 
-
-// function tester() {
-//   console.log("test function called")
-//   var hideTest = window.confirm("hide?");
-//   if (hideTest) {
-//     console.log("hide confirmed");
-//     hideoptions.setAttribute("style", "display:none");
-//   }
-// };
